5f0076b7a2e78dd734c7d4989cc64095da9b2bc1
zhengsongchao <879962116@qq.com>
2017-04-17 16:13:56 +0800

    small change

--- src/app.js
+++ src/app.js
@@ -1,63 +1,68 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 
-import Breadcrumb from './components/breadcrumb'
+import Breadcrumb from './components/breadcrumb';
 
 const VueProgressBarOptions = {
-    color: '#13ce66',
-    failedColor: '#FF4949',
-    height: '4px'
-}
-import VueProgressBar from './libs/vue-progressbar'
-import VueAxiosProgressBarInterceptor from './libs/vue-axios-progressbar-interceptor'
-Vue.use(VueProgressBar, VueProgressBarOptions)
-Vue.use(VueAxiosProgressBarInterceptor)
+  color: '#13ce66',
+  failedColor: '#FF4949',
+  height: '4px'
+};
+import VueProgressBar from './libs/vue-progressbar';
+import VueAxiosProgressBarInterceptor
+  from './libs/vue-axios-progressbar-interceptor';
+Vue.use(VueProgressBar, VueProgressBarOptions);
+Vue.use(VueAxiosProgressBarInterceptor);
 
-import Mock from './mock'
-Mock.bootstrap()
+import Mock from './mock';
+Mock.bootstrap();
 
-import App from './app.vue'
-import routes from './router/routes'
-import './style.scss'
+import App from './app.vue';
+import routes from './router/routes';
+import './style.scss';
 
 // import 'plugins/'
-import 'mixin/'
+import 'mixin/';
 
-import store from './store/'
-Vue.use(VueRouter)
-Vue.use(ElementUI)
+import store from './store/';
+Vue.use(VueRouter);
+Vue.use(ElementUI);
 
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
+import NProgress from 'nprogress';
+// import 'nprogress/nprogress.css';
 
-Vue.component('db-breadcrumb', Breadcrumb)
+Vue.component('db-breadcrumb', Breadcrumb);
 
-export const router = new VueRouter({routes, mode: 'hash', linkActiveClass: 'active'})
+export const router = new VueRouter({
+  routes,
+  mode: 'hash',
+  linkActiveClass: 'active'
+});
 
 router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)) {
-        let user = JSON.parse(localStorage.getItem('user'))
-        if (!user) {
-            next({
-                path: '/login',
-                query: {
-                    redirect: to.fullPath
-                }
-            })
-        } else {
-            NProgress.start()
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    let user = JSON.parse(localStorage.getItem('user'));
+    if (!user) {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
         }
+      });
+    } else {
+      NProgress.start();
     }
-    next()
-})
+  }
+  next();
+});
 router.afterEach(transition => {
-    NProgress.done()
-})
+  NProgress.done();
+});
 new Vue({
-    render: h => h(App),
-    router,
-    store
-}).$mount('#app')
+  render: h => h(App),
+  router,
+  store
+}).$mount('#app');
