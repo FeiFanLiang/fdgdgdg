f442d88e151ef3bc955bf360b172461d38451fb0
zhengsongchao <879962116@qq.com>
2017-04-19 09:12:17 +0800

    m

---
+++ src/mock/Account.js
@@ -0,0 +1,42 @@
+import { LoginUsers } from '../resources/user';
+const API = {
+  login(config) {
+    let { username, password } = JSON.parse(config.data);
+    return new Promise((resolve, reject) => {
+      let user = null;
+      setTimeout(() => {
+        let hasUser = LoginUsers.some(u => {
+          if (u.username === username && u.password === password) {
+            user = JSON.parse(JSON.stringify(u));
+            user.password = undefined;
+            return true;
+          }
+        });
+
+        if (hasUser) {
+          resolve([
+            200,
+            {
+              code: 200,
+              msg: '请求成功!!!',
+              user
+            }
+          ]);
+        } else {
+          resolve([
+            200,
+            {
+              code: 500,
+              msg: '用户名或密码错误!!!'
+            }
+          ]);
+        }
+      }, Math.random() * 1000 + 1000);
+    });
+  }
+};
+export default {
+  bootstrap(mock) {
+    mock.onPost('/login').reply(API.login);
+  }
+};
--- src/mock/HotelArea.js
+++ src/mock/HotelArea.js
@@ -1,15 +1,15 @@
-import Mock from 'mockjs'
+import Mock from 'mockjs';
 const API = {
-    listByPArea() {},
-    listByQuery() {},
-    listByQue() {},
-    listById() {}
-}
+  listByPArea() {},
+  listByQuery() {},
+  listByQue() {},
+  listById() {}
+};
 export default {
-    bootstrap(mock) {
-        mock.onGet('/Hotel/HotelArea/pid/{PArea}').reply(API.listByPArea)
-        mock.onGet('/Hotel/HotelArea/query').reply(API.listByQuery)
-        mock.onGet('/Hotel/HotelArea/query/{que}').reply(API.listByQue)
-        mock.onGet('/Hotel/HotelArea/{id}').reply(API.listById)
-    }
-}
+  bootstrap(mock) {
+    mock.onGet('/Hotel/HotelArea/pid/{PArea}').reply(API.listByPArea);
+    mock.onGet('/Hotel/HotelArea/query').reply(API.listByQuery);
+    mock.onGet('/Hotel/HotelArea/query/{que}').reply(API.listByQue);
+    mock.onGet('/Hotel/HotelArea/{id}').reply(API.listById);
+  }
+};
