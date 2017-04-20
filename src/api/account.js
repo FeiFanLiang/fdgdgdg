35f1bff069c7aa030e41f136d48703266212f0af
zhengsongchao <879962116@qq.com>
2017-04-20 17:01:09 +0800

    m

--- src/api/account.js
+++ src/api/account.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import path from './api';
 export default {
   login(params) {
-    return axios.post(path.apiBaseUrl + 'Account/Login', { params: params });
+    return axios.post(path.apiBaseUrl + 'Account/Login', params);
   },
   register(params) {
-    return axios.post(path.apiBaseUrl + 'Account/Register', { params: params });
+    return axios.post(path.apiBaseUrl + 'Account/Register', params);
   },
   logout(params) {
-    return axios.post(path.apiBaseUrl + 'Account/Logout', { params: params });
+    return axios.post(path.apiBaseUrl + 'Account/Logout', params);
   }
 };
