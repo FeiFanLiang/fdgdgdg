f404200927c4de5d904dc8ba7e2c97e9f1bde93d
zhouyh <2830694169@qq.com>
2017-04-20 17:46:57 +0800

    replace real api

--- src/api/pay-company.js
+++ src/api/pay-company.js
@@ -2,21 +2,19 @@ import axios from 'axios';
 import path from './api';
 
 export default {
-  getList(params) {
-    return axios.get(path.apiBaseUrl + `Hotel/PayCompany/All`, {
-      params: params
-    });
-  },
-  getDetail(params) {
-    return axios.get(path.apiBaseUrl + `Hotel/PayCompany/${id}`, params);
-  },
-  addInfo(params) {
-    return axios.post(path.apiBaseUrl + `Hotel/PayCompany`, params);
-  },
-  editInfo(params) {
-    return axios.put(path.apiBaseUrl + `Hotel/PayCompany/${id}`, params);
-  },
-  delInfo(params) {
-    return axios.delete(path.apiBaseUrl + `Hotel/PayCompany/${id}`, params);
-  }
+    getList(params) {
+        return axios.get(path.apiBaseUrl + `Hotel/PayCompany/All`, params);
+    },
+    getDetail(id) {
+        return axios.get(path.apiBaseUrl + `Hotel/PayCompany/${id}`);
+    },
+    addInfo(params) {
+        return axios.post(path.apiBaseUrl + `Hotel/PayCompany`, params);
+    },
+    editInfo(params) {
+        return axios.put(path.apiBaseUrl + `Hotel/PayCompany/${params.ID}`, params);
+    },
+    delInfo(id) {
+        return axios.delete(path.apiBaseUrl + `Hotel/PayCompany/${id}`);
+    }
 };
