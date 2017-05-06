42b3ff2253e48640cb6a03c3b2850b59594750e9
zhengsongchao <879962116@qq.com>
2017-05-06 11:36:20 +0800

    m

--- src/components/breadcrumb.vue
+++ src/components/breadcrumb.vue
@@ -2,7 +2,7 @@
   <div class="">
     <el-breadcrumb separator="/">
       <span class="db-breadcrumb-loc">位置：</span>
-      <el-breadcrumb-item v-for="item in breadcrumbs" :to="item">
+      <el-breadcrumb-item v-for="item in breadcrumbs" :to="item" v-if="item.name!=='酒店信息编辑'">
         {{ item.name }}
       </el-breadcrumb-item>
     </el-breadcrumb>
@@ -22,9 +22,15 @@ export default {
       breadcrumbs: []
     };
   },
+  watch:{
+    breadcrumbs(){
+        console.log(this.breadcrumbs)
+    }
+  },
 
   mounted() {
     this.breadcrumbs = (this.$parent && this.$parent.$route && this.$parent.$route.matched) || [];
+
   }
 };
 </script>
