2019a91294dac09d4daf89558fcfc3761d4b6c3a
zhengsongchao <879962116@qq.com>
2017-05-08 14:55:13 +0800

    m

--- src/pages/hotel/hotel-room/hotel-room.vue
+++ src/pages/hotel/hotel-room/hotel-room.vue
@@ -167,6 +167,27 @@
           </div>
         </el-col>
       </el-row>
+      <div class="line">
+
+      </div>
+      <el-table
+     :data="tableData"
+     style="width: 100%">
+     <el-table-column
+       prop="date"
+       label="日期"
+       width="180">
+     </el-table-column>
+     <el-table-column
+       prop="name"
+       label="姓名"
+       width="180">
+     </el-table-column>
+     <el-table-column
+       prop="address"
+       label="地址">
+     </el-table-column>
+   </el-table>
     </el-form>
     <span slot="footer" class="dialog-footer">
         <el-button @click="sonFormDialogVisible = false">取 消</el-button>
@@ -190,6 +211,23 @@ export default {
   },
   data() {
     return {
+      tableData: [{
+           date: '2016-05-02',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1518 弄'
+         }, {
+           date: '2016-05-04',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1517 弄'
+         }, {
+           date: '2016-05-01',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1519 弄'
+         }, {
+           date: '2016-05-03',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1516 弄'
+         }],
       breakfastTypes: [{
         name: '未定',
         value: 0
