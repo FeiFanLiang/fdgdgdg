3b5d8ed6687a8ca58065b7e9088caa56ff6443ee
zhouyh <2830694169@qq.com>
2017-05-08 11:52:43 +0800

    z

--- src/api/role.js
+++ src/api/role.js
@@ -2,35 +2,27 @@ import axios from 'axios';
 import path from './api';
 
 export default {
-    list() {
+    getRoleList() {
         return axios.get(path.apiBaseUrl + 'System/Role');
     },
-    roleListByRolsName(rolsName) {
+    getUserList() {
+        return axios.get(path.apiBaseUrl + 'System/User');
+    },
+    getUsernameByRole(rolsName) {
         return axios.get(path.apiBaseUrl + `System/Role/${rolsName}`);
     },
-    roleListByUserName(userName) {
-        return axios.get(path.apiBaseUrl + `System/Role/ForUser/${userName}`);
-    },
+    // roleListByUserName(userName) {
+    //     return axios.get(path.apiBaseUrl + `System/Role/ForUser/${userName}`);
+    // },
     addUserNameByRolsName(rolsName, userName, params) {
         return axios.post(
             path.apiBaseUrl + `System/Role/${rolsName}/${userName}`,
             params
         );
     },
-    addRolsNameByUserName(userName, rolsName, params) {
-        return axios.post(
-            path.apiBaseUrl + `System/Role/ForUser/${userName}/${rolsName}`,
-            params
-        );
-    },
     deleteByUserName(rolsName, userName) {
         return axios.delete(
             path.apiBaseUrl + `System/Role/${rolsName}/${userName}`
-        );
-    },
-    deleteByRolsName(rolsName, userName) {
-        return axios.delete(
-            path.apiBaseUrl + `System/Role/ForUser/${userName}/${rolsName}`
         );
     }
 };
