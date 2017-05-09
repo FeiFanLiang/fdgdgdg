0c1fbaad8b6a3eb6788595b09b348278b8a6a8bd
zhengsongchao <879962116@qq.com>
2017-05-09 17:54:27 +0800

    What:
    Why:
    
    *
    
    How:
    
    *

--- src/api/role.js
+++ src/api/role.js
@@ -2,24 +2,24 @@ import axios from 'axios';
 import path from './api';
 
 export default {
-    list() {
-        return axios.get(path.apiBaseUrl + 'System/Role');
-    },
-    userNameListByRolesName(rolsName) {
-        return axios.get(path.apiBaseUrl + `System/Role/${rolsName}`);
-    },
-    roleListByUserName(userName) {
-        return axios.get(path.apiBaseUrl + `System/Role/ForUser/${userName}`);
-    },
-    addUserNameByRolsName(rolsName, userName, params) {
-        return axios.post(
-            path.apiBaseUrl + `System/Role/${rolsName}/${userName}`,
-            params
-        );
-    },
-    deleteUserNameByRolesName(rolsName, userName) {
-        return axios.delete(
-            path.apiBaseUrl + `System/Role/${rolsName}/${userName}`
-        );
-    }
+  list() {
+    return axios.get(path.apiBaseUrl + 'System/Role');
+  },
+  userNameListByRolesName(rolsName) {
+    return axios.get(path.apiBaseUrl + `System/Role/${rolsName}`);
+  },
+  roleListByUserName(userName) {
+    return axios.get(path.apiBaseUrl + `System/Role/ForUser/${userName}`);
+  },
+  addUserNameByRolsName(rolsName, userName, params) {
+    return axios.post(
+      path.apiBaseUrl + `System/Role/${rolsName}/${userName}`,
+      params
+    );
+  },
+  deleteUserNameByRolesName(rolsName, userName) {
+    return axios.delete(
+      path.apiBaseUrl + `System/Role/${rolsName}/${userName}`
+    );
+  }
 };
