be553545279fd977183651d60021757195ec56c5
zhouyh <2830694169@qq.com>
2017-05-15 17:05:15 +0800

    z

--- src/pages/hotel/hotel-policy/hotel-policy.vue
+++ src/pages/hotel/hotel-policy/hotel-policy.vue
@@ -425,12 +425,9 @@ export default {
             const hotelID = this.$route.params.ID;
             const res = await hotelPolicyApi.listByHotelID(hotelID);
             this.hotelpolicy = res.data;
-<<<<<<< HEAD
-            this.expandRowKeys.push(this.hotelpolicy[0].ID);
-=======
+
             this.expandRowKeys.length=0;
             this.expandRowKeys.push(this.hotelpolicy[0].ID)
->>>>>>> 1d1d9cea4f14acfc6b1461376f51118b3ef33911
         },
         beforeAvatarUpload(file) {
             const isJPG = file.type === 'image/jpeg' || file.type === 'image/png';
@@ -439,11 +436,8 @@ export default {
             }
             return isJPG;
         },
-<<<<<<< HEAD
-        async handleExpand(row, expanded) {
-=======
+
       async  handleExpand(row, expanded) {
->>>>>>> 1d1d9cea4f14acfc6b1461376f51118b3ef33911
             if (expanded) {
                 this.expandRowKeys.length = 0;
                 this.expandRowKeys.push(row.ID);
