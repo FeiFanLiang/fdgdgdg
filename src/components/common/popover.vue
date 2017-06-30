21851ae34fb6dbba6ee513ee75f145dcf180bd2b
zhengsongchao <879962116@qq.com>
2017-06-30 08:41:32 +0800

    s

---
+++ src/components/common/popover.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div class="">
+      <span v-popover:popover>{{popoverText  }}</span>
+      <el-popover
+      ref="popover"
+      placement="bottom"
+      title="标题"
+      width="200"
+      trigger="click"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+      </el-popover>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    popoverText: ''
+  }
+}
+</script>
+
+<style lang="css">
+</style>
