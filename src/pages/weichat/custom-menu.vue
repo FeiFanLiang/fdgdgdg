0165beb9a49622dfc22e908857131c835e9d4525
cjy <1778533952@qq.com>
2017-06-30 16:12:01 +0800

    cjy

--- src/pages/weichat/custom-menu.vue
+++ src/pages/weichat/custom-menu.vue
@@ -1,19 +1,23 @@
 <template lang="html">
 <div id="customMenu">
-    <div style="width:200px;height:600px;border:solid 1px #A9A9A9;float:left;">
+    <div style="width:300px;height:600px;border:solid 1px #A9A9A9;float:left;">
         <p style="background-color:#EAEAEA;margin:0;padding:10px;">
            <el-row>
-               <el-col :span="22">菜单管理</el-col>
-               <el-col :span="2"><i class="el-icon-plus"></i></el-col>
+               <el-col :span="21">菜单管理</el-col>
+               <el-col :span="1">
+                   <el-button size="mini" @click="addMenu()" >
+                       <i class="el-icon-plus"></i>
+                   </el-button>
+               </el-col>
            </el-row>
         </p>
-        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false" :render-content="renderContent"></el-tree>
+        <el-tree :data="eltreeData" :props="defaultProps" node-key="id" :expand-on-click-node="false" :render-content="renderContent"></el-tree>
     </div>
     <div style="width:600px;height:600px;border:solid 1px #A9A9A9;float:left;">
         <p style="background-color:#EAEAEA;margin:0;padding:10px;">
            设置动作--子菜单
         </p>
-        <div>
+        <div v-if="editShow">
             <p style="padding:10px;">
                 <el-row>
                     <el-col :span="4">子菜单名称</el-col>
@@ -36,7 +40,7 @@
                 </el-row>
                 <div style="border:solid 1px #A9A9A9;margin:10px;height:300px;">
                     <p style="padding:0px;margin:0px;">
-                        <el-menu :default-active="activeIndex2" class="el-menu-demo"
+                        <el-menu :default-active="activeIndex" class="el-menu-demo"
                          mode="horizontal" @select="handleSelect">
                             <el-menu-item index="1"><i class="el-icon-menu">图文消息</i></el-menu-item>
                             <el-menu-item index="2"><i class="el-icon-picture">图片</i></el-menu-item>
@@ -63,171 +67,190 @@
                     </div>
                 </div>
             </p>
+            <el-button style="margin:10px;">保存</el-button>
         </div>
+        <div v-if="editShow2" style="padding:35%">已有子菜单，无法设置动作</div>
+        <div v-if="editShow3" style="padding:40%">暂无内容</div>
     </div>
 </div>
 </template>
 <script>
+let id = 1000;
 export default {
-  data() {
-    return {
-      input: '',
-      radio: '',
-      activeIndex2: '1',
-      imageUrl: '',
-      data: [
-        {
-          id: 1,
-          label: '美票旅游',
-          /*children: [{
-            label: '机票',
-            label: '酒店',
-            label: '机场专车',
-            label: '自由行',
-            label: '跟团游'
-          }]*/
-          children: [
-            {
-              id: 4,
-              label: '机票'
-            },
-            {
-              id: 5,
-              label: '酒店'
-            },
-            {
-              id: 6,
-              label: '机场专车'
-            },
-            {
-              id: 7,
-              label: '自由行'
-            },
-            {
-              id: 8,
-              label: '跟团游'
+    data() {
+        return {
+            editShow:false,
+            editShow2:false,
+            editShow3:true,
+            input: '',
+            radio: '',
+            activeIndex: '1',
+            imageUrl: '',
+            eltreeData: [{
+                id: 1,
+                label: '美票旅游',
+                children: [{
+                    id: 4,
+                    label: '机票',
+                },{
+                    id: 5,
+                    label: '酒店',
+                },{
+                    id: 6,
+                    label: '机场专车',
+                },{
+                    id: 7,
+                    label: '自由行',
+                },{
+                    id: 8,
+                    label: '跟团游',
+                }]
+            }, {
+                id: 2,
+                label: '我的',
+            }, {
+                id: 3,
+                label: '关于美票',
+                children: [{
+                    id: 9,
+                    label: '微店商城'
+                }, {
+                    id: 10,
+                    label: '美票说'
+                }]
+            }],
+            defaultProps: {
+                children: 'children',
+                label: 'label'
             }
-          ]
-        },
-        {
-          id: 2,
-          label: '我的'
-        },
-        {
-          id: 3,
-          label: '关于美票',
-          children: [
-            {
-              id: 9,
-              label: '微店商城'
-            },
-            {
-              id: 10,
-              label: '美票说'
-            }
-          ]
         }
-      ],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      }
+    },
+    methods: {
+        handleNodeClick(data) {
+            console.log(data)
+        },
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath)
+        },
+        handleAvatarSuccess(res, file) {
+            this.imageUrl = URL.createObjectURL(file.raw)
+        },
+        beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg'
+            const isLt2M = file.size / 1024 / 1024 < 2
+
+            if (!isJPG) {
+                this.$message.error('上传头像图片只能是 JPG 格式!')
+            }
+            if (!isLt2M) {
+                this.$message.error('上传头像图片大小不能超过 2MB!')
+            }
+            return isJPG && isLt2M
+        },
+
+        addMenu(){
+            this.eltreeData.push({ id: id++, label: '主菜单名称' });
+        },
+
+        append(store, data) {
+            store.append({ id: id++, label: '子菜单名称' }, data);
+        },
+
+        remove(store, data) {
+            store.remove(data);
+        },
+
+        edit(store,data){
+            /*console.log(store)*/
+            console.log(data)
+            if(data.hasOwnProperty('children')){
+                this.editShow = false;
+                this.editShow2 = true;
+                this.editShow3 = false;
+                this.$prompt('请输入菜单名称', '提示', 
+                { 
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    inputValue: data.label,
+                    inputPattern: /^[\u4e00-\u9fa5A-Za-z0-9]{2,8}$/, 
+                    inputErrorMessage: '只能是中文英文数字下划线 且长度为2-8',
+                    callback:((action, value)=> {if(action == 'confirm'){data.label= value.inputValue;}})
+                }).then(({ value }) => {
+                    this.$message({
+                        type: 'success',
+                        message: value
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '取消输入'
+                    });       
+                });
+            }else{
+                this.editShow = true;
+                this.editShow2 = false;
+                this.editShow3 = false;
+                this.input = data.label;
+            }
+        },
+
+        renderContent(h, { node, data, store }) {
+            return (
+            <span>
+                <span>
+                <span>{node.label}</span>
+                </span>
+                <span style="float: right; margin-right: 20px">
+                <el-button size="mini" on-click={ () => this.append(store, data) }><i class="el-icon-plus"></i></el-button>
+                <el-button size="mini" on-click={ () => this.remove(store, data) }><i class="el-icon-delete"></i></el-button>
+                <el-button size="mini" on-click={ () => this.edit(store, data) }><i class="el-icon-edit"></i></el-button>
+                </span>
+            </span>);
+        }
     }
-  },
-  methods: {
-    handleNodeClick(data) {
-      console.log(data)
-    },
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleAvatarSuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw)
-    },
-    beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg'
-      const isLt2M = file.size / 1024 / 1024 < 2
-
-      if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!')
-      }
-      if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!')
-      }
-      return isJPG && isLt2M
-    },
-    append(store, data) {
-      store.append({ id: id++, label: 'testtest', children: [] }, data)
-    },
-
-    remove(store, data) {
-      store.remove(data)
-    },
-
-    renderContent(h, { node, data, store }) {
-      // let a = [
-
-      // ]
-      return (
-        <span>
-          <span>
-            <span>{node.label}</span>
-          </span>
-          <span style="float: right; margin-right: 20px">
-            <el-button size="mini" on-click={() => this.append(store, data)}>
-              Append
-            </el-button>
-            <el-button size="mini" on-click={() => this.remove(store, data)}>
-              Delete
-            </el-button>
-          </span>
-        </span>
-      )
-    }
-  }
 }
 </script>
 <style lang="scss">
-#customMenu{
-    .el-menu--horizontal>.el-menu-item:hover{
+#customMenu {
+    .el-menu--horizontal>.el-menu-item:hover {
         border-bottom: solid 0px white;
     }
-    .el-menu--horizontal .el-menu-item:hover{
+    .el-menu--horizontal .el-menu-item:hover {
         background-color: white;
     }
     .el-menu {
         background-color: white;
     }
     .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-    height: 178px;
-    margin: 20px;
-    margin-left: 50px;
-    float:left;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #20a0ff;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 18px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
-  .el-upload__text{
-      color: lightgrey;
-      padding-top: 10px;
-  }
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+        height: 178px;
+        margin: 20px;
+        margin-left: 50px;
+        float: left;
+    }
+    .avatar-uploader .el-upload:hover {
+        border-color: #20a0ff;
+    }
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 18px;
+        line-height: 178px;
+        text-align: center;
+    }
+    .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
+    }
+    .el-upload__text {
+        color: lightgrey;
+        padding-top: 10px;
+    }
 }
 </style>
