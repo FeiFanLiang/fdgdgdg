85255e21781b8a5c995ad7b668597361a371cb97
zhouyh <2830694169@qq.com>
2017-07-29 16:31:55 +0800

    z

--- src/router/role.js
+++ src/router/role.js
@@ -1,15 +1,27 @@
 import Pages from '../pages'
+
+const beforeEnter = (to, from, next) => {
+  let user = JSON.parse(localStorage.getItem('user'));
+  if (user.username === 'admin') {
+    next();
+  } else {
+    alert("您没有权限进入该页面！")
+    next({
+      path: '/404'
+    });
+  }
+};
+
 export default {
   path: 'role',
   component: Pages.Abstract,
   name: '角色管理',
   redirect: '/role/role',
   iconClass: 'el-icon-setting',
-  children: [
-    {
-      path: 'role',
-      component: Pages.Role,
-      name: '角色管理'
-    }
-  ]
+  children: [{
+    path: 'role',
+    component: Pages.Role,
+    beforeEnter,
+    name: '角色管理'
+  }]
 }
