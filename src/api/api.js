ed2e9324b591fdaf3da503e9edb48e48f08a91b7
miaoqiang <wxsgtkdg@msn.com>
2017-08-09 16:56:49 +0800

    外网URL适配

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,21 @@
 const base = 'http://192.168.10.95:8500/' // 正式
+const outsideBase = 'http://http://60.209.152.178:8500/'
 //  const base='http://192.168.10.95:8501/' //测试
 // const base='http://weixinredirect.qdjp.cn/' //对外
 // const base='http://liukai.iok.la/'
 
+
+console.log(window.location.host)
+let url = ''
+if (window.location.host.indexOf('192.168') > -1) {
+  url = base
+} else {
+  url = outsideBase
+}
+
+
 export default {
-  apiBaseUrl: base,
-  uploadUrl: base + 'Hotel/Image/',
-  imageUrl: base + 'upload/'
+  apiBaseUrl: url,
+  uploadUrl: url + 'Hotel/Image/',
+  imageUrl: url + 'upload/'
 }
