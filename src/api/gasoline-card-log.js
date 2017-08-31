011c6501fb5eabf8013a35dc32b72b83e9156db6
zhouyh <2830694169@qq.com>
2017-08-31 15:22:06 +0800

    z

---
+++ src/api/gasoline-card-log.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import path from './api'
+const base = path.apiBaseUrl + 'ParkSale/GasolineCardLog/'
+
+export default {
+        list(params) {
+            return axios.get(base + 'list', {params:params})
+        },
+        detail(id) {
+            return axios.get(base + id)
+        },
+        add(params) {
+            return axios.post(base, params)
+        },
+        edit(id, params) {
+            return axios.put(base + id, params)
+        },
+        del(id) {
+            return axios.delete(base + id)
+        }
+}
