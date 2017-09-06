3231fd3ec4570a2fbe780dc7cde6289989742e79
zhouyh <2830694169@qq.com>
2017-09-06 11:08:10 +0800

    z

--- src/pages/fleet/gasoline-card-log/gasoline-card-log.vue
+++ src/pages/fleet/gasoline-card-log/gasoline-card-log.vue
@@ -14,9 +14,6 @@
             <el-col :span="10">
                 <el-button type="primary" @click="gasolineSearch(filters)">搜索</el-button>
                 <el-button type="primary" @click="clear()">清空</el-button>
-                <el-button type="primary" @click="clickAddBtn">
-                    创建
-                </el-button>
             </el-col >
         </el-row>
         <el-table :data="list" ref="table" style="width: 100%" element-loading-text="拼命加载中" v-loading="loading" border>
@@ -30,61 +27,28 @@
                 </template>
             </el-table-column>
             <el-table-column prop="ID" label="ID"></el-table-column>
+            <el-table-column label="类型" show-overflow-tooltip>
+                <template scope="scope">
+                    <span v-if="scope.row.UseType === 0">充值</span>
+                    <span v-if="scope.row.UseType === 1">加油</span>
+                </template>
+            </el-table-column>
             <el-table-column prop="DateTime" label="加油时间" show-overflow-tooltip></el-table-column>
-            <el-table-column prop="GasolineCardID" label="油卡编号"></el-table-column>
+            <el-table-column prop="cardNum" label="油卡编号"></el-table-column>
             <el-table-column prop="Total" label="总额"></el-table-column>
             <el-table-column prop="Balance" label="余额"></el-table-column>
-            <el-table-column label="操作" width="150">
+            <!-- <el-table-column label="操作" width="150">
 <template scope="scope">
     <el-button size="small" @click="clickEditBtn(scope.$index, scope.row)">
         编辑</el-button>
     <DeleteButton api="gasolineCardLogApi" @successCallBack="fetchData" :id="scope.row.ID"></DeleteButton>
 </template>
-            </el-table-column>
+            </el-table-column> -->
         </el-table>
         <div class="pagination-wrapper">
             <el-pagination layout="total, sizes, prev, pager, next, jumper" :page-sizes="[10, 20, 30]" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-size="pageSize" :total="count">
             </el-pagination>
         </div>
-        <el-dialog :title="form.ID?'编辑加油卡充值记录':'添加加油卡充值记录'" v-model="showDialog" @close="resetForm('form')">
-            <el-form ref="form" :model="form" :rules="rules" label-width="110px">
-                <el-row :gutter="24">
-                    <el-col :span="12">
-                        <el-form-item label="加油时间" prop="DateTime">
-                            <el-date-picker v-model="form.DateTime" type="datetime" placeholder="选择加油时间" :picker-options="pickerOptions" style="width:100%"></el-date-picker>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                        <el-form-item label="油卡编号" prop="GasolineCardID">
-                            <el-select v-model="form.GasolineCardID" placeholder="请选择">
-                                <el-option
-                                v-for="(item,index) in cardList"
-                                :key="index"
-                                :label="item.CardNum"
-                                :value="item.ID">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-                <el-row :gutter="24">
-                    <el-col :span="12">
-                        <el-form-item label="总额" prop="Total">
-                            <el-input placeholder="请输入总额" v-model="form.Total"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                        <el-form-item label="余额" prop="Balance">
-                            <el-input placeholder="请输入余额" v-model="form.Balance"></el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-            <el-button @click="showDialog = false">取 消</el-button>
-            <el-button type="primary" @click="submitForm()" :loading="!isEditable">{{isEditable?'确 定':'提交中'}}</el-button>
-            </span>
-        </el-dialog>
     </div>
 </template>
 <script>
@@ -98,12 +62,6 @@ export default {
   },
   data() {
     return {
-      rules: {
-        GasolineCardID: [{ required: true, message: '请选择油卡编号' }],
-        DateTime: [{ required: true, message: '请选择加油时间' }],
-        Total: [{ required: true, message: '请输入总额' }],
-        Balance: [{ required: true, message: '请输入余额' }]
-      },
       filters: {
         gasolineCardID: ''
       },
@@ -113,17 +71,8 @@ export default {
       pageSize: 10,
       count: 0,
       loading: false,
-      showDialog: false,
       disabled: false,
-      form: {
-        GasolineCardID:'',
-        DateTime: '',
-        Total:'',
-        Balance:'',
-        UseType:0
-      },
       pickerOptions: {},
-      isEditable:true,
       cardList:[]
     }
   },
@@ -164,6 +113,11 @@ export default {
       try {
         const res = await gasolineCardLogApi.list(options)
         _self.list = res.data.Data
+        _self.list.forEach(function(item1){
+            _self.cardList.forEach(function(item2){
+                if(item1.GasolineCardID === item2.ID) item1.cardNum = item2.CardNum
+            })
+        })
         _self.count = res.data.Count
         _self.loading = false
       } catch (e) {
@@ -182,87 +136,6 @@ export default {
     handleCurrentChange(val) {
       this.currentPage = val
       this.fetchData(this.currentPage)
-    },
-    clickAddBtn() {
-      const _self = this
-      this.form = {
-        GasolineCardID:'',
-        DateTime: '',
-        Total:'',
-        Balance:'',
-        UseType:0
-      }
-      _self.showDialog = true
-    },
-    async clickEditBtn($index, row) {
-      const _self = this
-      try {
-        const res = await gasolineCardLogApi.detail(row.ID)
-        _self.form = res.data.Data
-        _self.showDialog = true
-      } catch (e) {
-        console.error(e)
-      }
-    },
-    submitForm() {
-      const _self = this
-      if (_self.form.ID) {
-        _self.editSave()
-      } else {
-        _self.addSave()
-      }
-    },
-    async addSave() {
-      const _self = this
-      _self.$refs['form'].validate(async valid => {
-        if (valid) {
-          try {
-            _self.isEditable = false
-            _self.form.DateTime ? (_self.form.DateTime = new Date(_self.form.DateTime).Format('yyyy-MM-dd hh:mm:ss')): ''
-            await gasolineCardLogApi.add(_self.form)
-            _self.fetchData()
-            _self.$refs['form'].resetFields()
-            _self.showDialog = false
-            _self.$message({
-              message: '保存成功',
-              type: 'success'
-            })
-          } catch (e) {
-            console.error(e)
-            _self.$message.error('添加失败!!!')
-          } finally {
-            _self.isEditable = true
-          }
-        } else {
-          return false
-        }
-      })
-    },
-    async editSave() {
-      const _self = this
-      _self.$refs['form'].validate(async valid => {
-        if (valid) {
-          try {
-            _self.isEditable = false
-            _self.form.DateTime ? (_self.form.DateTime = new Date(_self.form.DateTime).Format('yyyy-MM-dd hh:mm:ss')): ''
-            await gasolineCardLogApi.edit(_self.form.ID, _self.form)
-            _self.fetchData()
-            _self.$refs['form'].resetFields()
-            _self.showDialog = false
-            _self.$message({
-              message: '编辑成功',
-              type: 'success'
-            })
-          } catch (e) {
-            console.error(e)
-            _self.$message.error('编辑失败!!!')
-          } finally {
-            _self.isEditable = true
-          }
-        } else {
-          return false
-        }
-      })
     },
     channelChange() {
       if (this.form.Channel == 0) {
