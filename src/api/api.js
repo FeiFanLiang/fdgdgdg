245d580c94c329ced053ed99857c646ca4351ede
zhengsongchao <879962116@qq.com>
2017-09-30 14:32:45 +0800

    s

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // const base = 'http://192.168.10.95:8500/' // 正式
-const outsideBase = 'http://60.209.152.178:8500/'
+const outsideBase = 'http://123.235.27.78:59510/'
 //  const base='http://60.209.152.178:8501/' //测试
 // const base='http://weixinredirect.qdjp.cn/' //对外 微信接口
 // const base='http://liukai.iok.la/'
---
+++ src/main-copy.js
@@ -0,0 +1,119 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import './libs/date'
+import { Breadcrumb } from 'components'
+import DeleteButton from 'components/common/delete-button.vue'
+import CustomTable from 'components/common/custom-table.vue'
+import CustomTableCopy from 'components/common/custom-table-copy.vue'
+import CustomSearch from 'components/common/custom-search.vue'
+
+axios.defaults.withCredentials = true
+
+axios.defaults.timeout = 3 * 60 * 1000
+
+// const VueProgressBarOptions = {
+//   color: '#13ce66',
+//   failedColor: '#FF4949',
+//   height: '4px'
+// };
+// import VueProgressBar from './libs/vue-progressbar';
+// import VueAxiosProgressBarInterceptor from './libs/vue-axios-progressbar-interceptor';
+// Vue.use(VueProgressBar, VueProgressBarOptions);
+// Vue.use(VueAxiosProgressBarInterceptor);
+
+import App from './app.vue'
+// import routes from './router'
+import routes, { getPermissionRoute } from './router'
+
+import './styles/index.scss'
+
+// import 'plugins/'
+import 'mixin/'
+
+// import store from './store/'
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+
+import NProgress from 'nprogress'
+
+Vue.component('db-breadcrumb', Breadcrumb)
+Vue.component('DeleteButton', DeleteButton)
+Vue.component('CustomTable', CustomTable)
+Vue.component('CustomTableCopy', CustomTableCopy)
+Vue.component('CustomSearch', CustomSearch)
+
+export const router = new VueRouter({
+  routes,
+  mode: 'history',
+  linkActiveClass: 'active'
+})
+router.addRoutes(getPermissionRoute('admin')) // 动态添加可访问路由表
+router.options.routes = [
+  ...router.options.routes,
+  ...getPermissionRoute('admin')
+]
+console.log(router.options.routes)
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    let user = JSON.parse(localStorage.getItem('user'))
+    if (!user) {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      NProgress.start()
+    }
+  }
+  next()
+})
+router.afterEach(transition => {
+  NProgress.done()
+})
+
+axios.interceptors.request.use(
+  function (config) {
+    // if (config.url.indexOf('weixinredirect.qdjp.cn') > -1) {
+    //   config.withCredentials = false
+    // }
+    // console.log(config)
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+axios.interceptors.response.use(
+  function (response) {
+    if (response.status === 320) {
+      router.replace({
+        name: 'login',
+        query: { redirect: router.currentRoute.fullPath }
+      })
+    }
+    return response
+  }
+  // function(error) {
+  //    if(error&&error.stack&&typeof error.stack==='string'&&error.stack.indexOf('XMLHttpRequest.handleError')>0){
+  //     // VueRouter.push({
+  //     //   path:'login'
+  //     // })
+  //    }else{
+  //     return Promise.reject(error);
+  //    }
+
+  // }
+)
+
+new Vue({
+  render: h => h(App),
+  router
+  // store
+}).$mount('#app')
