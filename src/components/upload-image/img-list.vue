e3787b034de4f3652f534d8806563c3fb7fda6e7
zhengsongchao <879962116@qq.com>
2017-11-14 13:20:45 +0800

    s

--- src/components/upload-image/img-list.vue
+++ src/components/upload-image/img-list.vue
@@ -9,7 +9,7 @@
       </div>
     </li>
   </ul>
-  <el-dialog v-model="dialogVisible" size="small" :modal-append-to-body="false" :append-to-body="true">
+  <el-dialog v-model="dialogVisible" size="small" :modal-append-to-body="false" :modal="false" custom-class="custom-class">
     <el-form :model="form" ref="form" label-width="130px">
       <img width="100%" :src="form.dialogImageUrl" alt="">
     </el-form>
@@ -52,6 +52,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.custom-class{
+  border:4px solid 
+}
 #list {
   .el-upload--picture-card {
     display: none !important;
