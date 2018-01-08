58bd2c0495a0dafb0fe2da2129213fbabd32c39e
zhengsongchao <879962116@qq.com>
2018-01-08 16:57:08 +0800

    s

--- src/api/air-informations.js
+++ src/api/air-informations.js
@@ -2,15 +2,14 @@ import axios from 'axios'
 import path from './api'
 const base = path.apiBaseUrl + 'ParkSale/AirInformations/GetAirInfo'
 export default {
-  listAll (params) {
-     return axios.get(base , {
-      params: params
-    })
-  }
+    listAll(params) {
+        return axios.get(base, {
+            params: params
+        })
+    }
 }
 
-
- /*  "FlightNo": "TZ168",//航班号
+/*  "FlightNo": "TZ168",//航班号
   "TakeOffTime": "2017-07-25 02:20:00",//起飞时间
   "ArrivalTime": "2017-07-25 07:43:00",//到达时间
   "Stat": "到达",//状态
